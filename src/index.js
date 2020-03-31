b2737514ea8ab45b875885313366150b740938c2
chenping <chenping@wecash.net>
2020-03-31 10:50:37 +0800

    asset management

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+import _ from 'lodash';
+import './style.css'
+
 function component() {
   const element = document.createElement('div');
 
   // Lodash ( 目前通过一个script 脚本引入)，对于执行这一行是必须的
-  element.innerHTML = _.join(['Hello', 'webpack'], '');
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', ' webpack'], ' ');
+  element.classList.add('hello');
 
   return element;
 }
