19d357a96a619a9156261754b627dc88b90f44d9
chenping <chenping@wecash.net>
2020-03-31 11:39:54 +0800

    revert coding

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './image.png';
-import Data from './data.xml';
 
 function component() {
   const element = document.createElement('div');
@@ -9,15 +6,6 @@ function component() {
   // Lodash ( 目前通过一个script 脚本引入)，对于执行这一行是必须的
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', ' webpack'], ' ');
-  element.classList.add('hello');
-
-  // 将图片添加至现有的 div
-  let myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-  console.log(Data);
 
   return element;
 }
