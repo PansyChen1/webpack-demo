0f27bf8a0f06eaff5f21e5aec3ce82ff1ec7189d
chenping <chenping@wecash.net>
2020-03-31 11:13:23 +0800

    add file-loader

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
-import './style.css'
+import './style.css';
+import Icon from './image.png';
 
 function component() {
   const element = document.createElement('div');
@@ -8,6 +9,12 @@ function component() {
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', ' webpack'], ' ');
   element.classList.add('hello');
+
+  // 将图片添加至现有的 div
+  let myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 
   return element;
 }
